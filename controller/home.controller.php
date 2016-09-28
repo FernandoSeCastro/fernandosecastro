e8015b2175a306c9e54e4d92074b40382ec05fbc
FernandoSeCastro <fernandosecastro@univem.edu.br>
2016-09-28 16:45:15 -0300

    subindo

--- controller/home.controller.php
+++ controller/home.controller.php
@@ -1,15 +1,29 @@
-<?php 
-class HomeController{
-	
-	private $model;
-	public function __CONSTRUCT()
-	{
-	}
-	
-	public function Index()
-	{
-		require_once 'view/header.php';
-		require_once 'view/home/home.php';
-		require_once 'view/footer.php';		
-	}
+<?php 
+
+class HomeController{
+
+	
+
+	private $model;
+
+	public function __CONSTRUCT()
+
+	{
+
+	}
+
+	
+
+	public function Index()
+
+	{
+
+		require_once 'view/header.php';
+
+		require_once 'view/home/home.php';
+
+		require_once 'view/footer.php';		
+
+	}
+
 }
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once 'model/database.php';
 
-$controller = 'cliente';
+$controller = 'home';
 
 if(!isset($_REQUEST['c']))
 {
