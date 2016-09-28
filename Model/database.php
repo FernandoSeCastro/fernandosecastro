a015db305530dd55c47e1179d9f4e586fbd58788
FernandoSeCastro <fernandosecastro@univem.edu.br>
2016-09-28 02:56:17 -0300

    editando front

--- Model/database.php
+++ Model/database.php
@@ -5,7 +5,7 @@ class Database
 {
 	public static function Conectar()
 	{
-        $pdo = new PDO('mysql:host=localhost;dbname=teste;charset=utf8', 'root', '');
+        $pdo = new PDO('mysql:host=mysql.hostinger.com.br;dbname=u579165536_teste;charset=utf8', 'u579165536_fer', '123456');
         //Retornando o erro da conexão
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
